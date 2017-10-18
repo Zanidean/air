58f3ce4daec9639898538981ad616f6d16f91503
Zanidean` <`AZanidean@mhc.ab.ca`>
2017-10-18 15:08:02 -0600

    Add get-diversity

---
+++ R/get_diversity.R
@@ -0,0 +1,34 @@
+#'Pull data from LERS
+#'@param rows Select rows to cut data by
+#'@param type type of simpsondiversity index
+#'@export get_diversity
+#'
+get_diversity <- function(rows, species, inst, type){
+  if(missing(type)){type = "dominance"}
+  if(missing(inst)){inst = c()}
+
+  sdi2 <- function (col, data, type)
+  {
+    data$number2 <- data[col] * (data[col] - 1)
+    Numer <- sum(data$number2)
+    Denom <- sum(data[col]) * (sum(data[col]) - 1)
+    if (type == "dominance") {
+      DI <- 1 - (Numer/Denom)
+    }
+    else if (type == "reciprocal") {
+      DI <- 1/(Numer/Denom)
+    }
+    else if (type == "SI") {
+      DI <- Numer/Denom
+    }
+    return(DI)
+  }
+
+  df <- get_enrolment("Unique Student Static", c(species, rows), institutions = inst) %>%
+    group_by_(.dots = c(lapply(c("Academic Year", rows), as.symbol))) %>%
+    do(summarise(., Diversity = sdi2(col = "Unique Student Static",
+                            data = ., type = type)))
+
+
+  return(df)
+}
