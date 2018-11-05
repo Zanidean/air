3bd458b886694257a46d19feab9b962575d64ddf
Zanidean` <`AZanidean@mhc.ab.ca`>
2018-11-05 11:55:35 -0700

    oops

--- tests/testthat/test-data_types.R
+++ tests/testthat/test-data_types.R
@@ -2,8 +2,6 @@ context("Checking Returned Data Types")
 
 source("Load.R")
 
-uname <- "AZanidean"
-pwd <- "668Marinr"
 
 test_that("Returned Enrolment data is a tibble", {
   test_data <- get_enrolment("FLE", username = uname, password = pwd)
